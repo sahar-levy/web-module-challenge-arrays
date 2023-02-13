cf383a670f86371fd34c9b3e933e3dd8f68f5ec4
Sahar Levy <sahar@nogalesfundmgmt.com>
2023-02-13 14:02:59 -0800

    confirmed the array contained 31 flavors of ice cream

--- index.js
+++ index.js
@@ -63,9 +63,14 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(array){
+  if (array.length === 31){
+    return true;
+  } else {
+    return false;
+  }
  }
+ console.log('Task 2:', is31Flavors(originalFlavors));
 
 
 
